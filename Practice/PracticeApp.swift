6623277ffa6962c337f6801c5a945b4279a22d1c
Syed Raza <syed.raza011@gmail.com>
2023-07-31 13:26:36 -0400

    Initial Commit

---
+++ Practice/PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeApp.swift
+//  Practice
+//
+//  Created by Syed Raza on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
